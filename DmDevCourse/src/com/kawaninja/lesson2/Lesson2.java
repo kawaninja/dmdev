8ba472ce37e47c03b342cd4ae2678b1ea04fc17d
Aleksei <kawaninja@icloud.com>
2022-02-20 15:32:55 +0600

    changes

---
+++ DmDevCourse/src/com/kawaninja/lesson2/Lesson2.java
@@ -0,0 +1,36 @@
+package com.kawaninja.lesson2;
+
+public class Lesson2 {
+
+	public static void main(String[] args) {
+		int value = 26;
+
+		int temp;
+
+		temp = 567;
+
+		value = temp;
+
+		System.out.println(temp);
+
+		System.out.println(value);
+
+		double value2 = 78.26;
+
+		System.out.println(value2);
+
+		boolean valueBoolean = true;
+
+		System.out.println(valueBoolean);
+
+		char valueChar;
+
+//		System.out.println(valueChar);
+
+		valueChar = 'T';
+
+		System.out.println(valueChar);
+
+	}
+
+}
---
+++ DmDevCourse/src/com/kawaninja/lesson3/Lesson3Example.java
@@ -0,0 +1,14 @@
+package com.kawaninja.lesson3;
+
+public class Lesson3Example {
+
+	public static void main(String[] args) {
+		int value = 5;
+		int value2 = 3;
+		int value3 = value + value2;
+		
+		System.out.println(value3);
+
+	}
+
+}
